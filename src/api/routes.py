f454362119abababeefda0de64f86fc632de7b0c
FrancescaAmeche <121627825+FrancescaAmeche@users.noreply.github.com>
2023-10-18 17:10:29 +0000

    added signup, login, private

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,17 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "Lv414Lv426Sv324Pv319Fa714"  # Change this!
+jwt = JWTManager(app)
+
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +27,45 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    body = request.get_json()
+    new_user = User(email=body["email"],password=body["password"])
+    db.session.add(new_user)
+    db.session.commit()
+    print("I will sign in")
+    return jsonify(new_user.serialize), 200
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return jsonify({"msg": "The user doesnt exist"}), 401
+    
+    print(user)
+    print(user.serialize)
+    print(user.email)
+    print(user.password)
+
+    if password != user.password:
+        return jsonify({"msg": "Wrong password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/private", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 3001))
+    app.run(host='0.0.0.0', port=PORT, debug=True)
